a5d081c77e20d42cbeae9a170688f311d60d3391
Zach Fleischman <zach_fleischman@live.com>
2013-10-16 21:12:40 -0600

    computer players seek out targets and no longer get stuck

--- ThreadGameSkeleton/src/ComputerPlayer.java
+++ ThreadGameSkeleton/src/ComputerPlayer.java
@@ -13,14 +13,29 @@ public class ComputerPlayer extends Player {
 
 		location = new Location(rand.nextInt(GraphicsPanel.MAX_X), rand.nextInt(GraphicsPanel.MAX_Y));
 	}
+	
 
 	@Override
 	protected void handleMovement() {
+		
+		//first, try to grab a point
+		for (int dx = -1; dx <= 1; dx++) {
+			for (int dy = -1; dy <= 1; dy++) {
+				Location potential = new Location(location.getX() + dx, location.getY() + dy);
+				if (graphics.checkForPoint(potential)) {
+					points++;
+					location.move(dx, dy);
+					return;
+				}
+			}
+		}
+		//then, make a random, legal move
 		Random rand = new Random();
 		int dx = 0;
 		int dy = 0;
-		do {
-			int movement = rand.nextInt(4);
+		int movement = rand.nextInt(4);
+		for (int i = 0; i < 4; i++ ) {
+			movement = (movement + 1) % 4;
 			switch (movement) {
 			case 0:
 				dx = 1;
@@ -37,13 +52,10 @@ public class ComputerPlayer extends Player {
 			default:
 				break;
 			}
-			
-		} while (!isMovementValid(dx, dy));
-		
-		location.move(dx, dy);
-		
-		if (graphics.checkForPoint(location)) {
-			points++;
+			if (isMovementValid(dx, dy)) {
+				location.move(dx, dy);
+				break;
+			}
 		}
 		
 		
